1637e244628e681c22731aabfb25074c9bec91be
biel-igl <gabrielpires524@gmail.com>
2023-04-27 18:52:40 -0300

    feat: cria mock para sales

--- tests/unit/controllers/sales.conntroller.test.js
+++ tests/unit/controllers/sales.conntroller.test.js
@@ -4,10 +4,17 @@ const sinonChai = require("sinon-chai");
 const { expect } = chai;
 chai.use(sinonChai);
 
+const { salesControllerMock } = require('./mock/sales.controllers.mock')
+
 describe("testa as vendas na camada Controllers", () => {
   describe('com get', () => {
     it('é chamado o status com o código 200', () => {
-      sinon.stub(salesService, )
+      sinon
+        .stub(salesService, "findAll")
+        .resolves({ type: null, message: salesControllerMock });
     })
   })
+  afterEach(function () {
+    sinon.restore();
+  });
 });
